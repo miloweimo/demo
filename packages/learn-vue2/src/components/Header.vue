5889158785a95496e2bf73d4116df296050816d3
Wei Mo <miloweimo@outlook.com>
2021-09-22 16:37:59 +0800

    add Header

---
+++ packages/learn-vue2/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar dense flat app height="40" width="100%" color="#fff">
+    <span>{{ moment().format('YYYY-MM-DD HH:mm:ss') }}</span>
+    <v-spacer></v-spacer>
+    <span class="text-h1">{{ "Maintenance" }}</span>
+    <v-spacer></v-spacer>
+    <img src="../assets/shopworx.png" height="100%" />
+  </v-app-bar>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "Header",
+  data: function () {
+    return { moment };
+  },
+};
+</script>
+
+<style>
+
+</style>
