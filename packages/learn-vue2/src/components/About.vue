5e42ffb2ad350fc043bb9705ed0620aa7d44dbfc
Wei Mo <miloweimo@outlook.com>
2021-12-26 15:31:08 +0800

    暗色主题
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/learn-vue2/src/components/About.vue
+++ packages/learn-vue2/src/components/About.vue
@@ -9,8 +9,11 @@
         Left aligned text on ltr and right aligned on rtl.
       </p>
     </v-container>
-    <v-btn depressed elevation="2" outlined @click="handleme"
+    <v-btn depressed elevation="2" outlined class="primary" @click="handleme"
       >clickme 修改rtl文字阅读方向</v-btn
+    >
+    <v-btn depressed elevation="2" outlined class="primary" @click="handleTheme"
+      >clickme 切换暗色主题</v-btn
     >
   </div>
 </template>
@@ -23,6 +26,11 @@ export default {
       console.log(this.$vuetify, "翻转文字方向");
       this.$vuetify.rtl = !this.$vuetify.rtl;
     },
+    handleTheme(){
+      console.log("切换暗色/亮色");
+      this.$vuetify.theme.isDark = !this.$vuetify.theme.isDark
+      
+    }
   },
 };
 </script>
--- packages/learn-vue2/src/plugins/vuetify.js
+++ packages/learn-vue2/src/plugins/vuetify.js
@@ -3,11 +3,37 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
 const opts = {
   // rtl: true,
+  icons: {
+    iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  },
+  theme: {
+    themes: {
+      light:
+
+      {
+        primary: "#ffc107",
+        secondary: "#607d8b",
+        accent: "#ff5722",
+        error: "#ffeb3b",
+        warning: "#9c27b0",
+        info: "#4caf50",
+        success: "#8bc34a"
+      },
+      dark: {
+        primary: colors.blue.darken3,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+      },
+
+    }
+  }
 }
 
 export default new Vuetify(opts)
