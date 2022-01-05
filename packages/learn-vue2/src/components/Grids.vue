5744c823146e9ee8b669df8126ed4dd82a86ce31
Wei Mo <miloweimo@outlook.com>
2022-01-05 10:26:02 +0800

    part3
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/learn-vue2/src/components/Grids.vue
+++ packages/learn-vue2/src/components/Grids.vue
@@ -344,7 +344,116 @@
           <div class="item item-8">8</div>
           <div class="item item-9">9</div>
         </div>
-        <p>上图会先填满第一列，再填满第2列，所以3号项目在第一列，4号项目在第二列。8号项目和9号项目被挤到了第四列。</p>
+        <p>
+          上图会先填满第一列，再填满第2列，所以3号项目在第一列，4号项目在第二列。8号项目和9号项目被挤到了第四列。
+        </p>
+      </template>
+
+      <template>
+        <h4>f. justify-items 属性， align-items 属性， place-items 属性</h4>
+        <p>
+          justify-items属性设置单元格内容的水平位置（左中右），align-items属性设置单元格内容的垂直位置（上中下）。
+        </p>
+        <p>.container {</p>
+        <p>justify-items: start | end | center | stretch;</p>
+        <p>align-items: start | end | center | stretch;</p>
+        <p>}</p>
+        <hr />
+        <p>justify-items: start;</p>
+        <img
+          src="https://www.wangbase.com/blogimg/asset/201903/bg2019032516.png"
+          alt=" justify-items: start;"
+        />
+        <hr />
+        <p>
+          place-items属性是align-items属性和justify-items属性的合并简写形式。
+        </p>
+        <p>place-items: align-items justify-items;</p>
+      </template>
+
+      <template>
+        <h4>
+          g. justify-content 属性， align-content 属性， place-content 属性
+        </h4>
+        <p>
+          justify-content属性是整个内容区域在容器里面的水平位置（左中右），align-content属性是整个内容区域的垂直位置（上中下）。
+        </p>
+        <p>
+          justify-content: start | end | center | stretch | space-around |
+          space-between | space-evenly;
+        </p>
+        <p>
+          align-content: start | end | center | stretch | space-around |
+          space-between | space-evenly;
+        </p>
+        <hr />
+        <VueCodeHighlight>style="justify-content: center;"</VueCodeHighlight>
+        <div id="container" style="justify-content: center;">
+          <div class="item item-1">1</div>
+          <div class="item item-2">2</div>
+          <div class="item item-3">3</div>
+          <div class="item item-4">4</div>
+          <div class="item item-5">5</div>
+          <div class="item item-6">6</div>
+          <div class="item item-7">7</div>
+          <div class="item item-8">8</div>
+          <div class="item item-9">9</div>
+        </div>
+        <hr />
+        <p>
+          place-content属性是align-content属性和justify-content属性的合并简写形式。
+        </p>
+        <p>place-content: align-content justify-content</p>
+      </template>
+
+      <template>
+        <h4>
+          h. grid-auto-columns 属性， grid-auto-rows 属性
+        </h4>
+        <p>
+          在现有网格的外部。比如网格只有3列，但是某一个项目指定在第5行。这时，浏览器会自动生成多余的网格，以便放置项目。
+        </p>
+        <p>
+          grid-auto-columns属性和grid-auto-rows属性用来设置，浏览器自动创建的多余网格的列宽和行高
+        </p>
+        <p>它们的写法与grid-template-columns和grid-template-rows完全相同。</p>
+        <hr />
+        <p>style="grid-auto-rows:50px"</p>
+        <div id="container" style="grid-auto-rows:50px">
+          <div class="item item-1">1</div>
+          <div class="item item-2">2</div>
+          <div class="item item-3">3</div>
+          <div class="item item-4">4</div>
+          <div class="item item-5">5</div>
+          <div class="item item-6">6</div>
+          <div class="item item-7">7</div>
+          <div
+            class="item item-8"
+            style="grid-row-start:4; grid-column-start:2;"
+          >
+            8
+          </div>
+          <div
+            class="item item-9"
+            style="grid-row-start:5; grid-column-start:3;"
+          >
+            9
+          </div>
+        </div>
+        <hr />
+      </template>
+
+      <template>
+        <h4>
+          i. grid-template 属性， grid 属性
+        </h4>
+        <p>
+          grid-template属性是grid-template-columns、grid-template-rows和grid-template-areas这三个属性的合并简写形式。
+        </p>
+        <p>
+          grid属性是grid-template-rows、grid-template-columns、grid-template-areas、
+          grid-auto-rows、grid-auto-columns、grid-auto-flow这六个属性的合并简写形式
+        </p>
       </template>
     </template>
 
