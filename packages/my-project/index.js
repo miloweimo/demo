537490e9b2fb46d012abaad76a5b9e29cb08c770
Wei Mo <miloweimo@outlook.com>
2023-08-22 14:21:24 +0800

    加依赖
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/my-project/index.js
+++ packages/my-project/index.js
@@ -1,3 +1,5 @@
 const dayjs = require('dayjs');
+const _ = require('lodash');
 
-console.log('my-project heloo', dayjs().format('YYYY-MM-DD HH:mm:ss'));
+console.log('my-project dayjs', dayjs().format('YYYY-MM-DD HH:mm:ss'));
+console.log('my-project lodash', _.camelCase('Foo Bar'));
