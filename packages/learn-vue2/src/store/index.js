0492633dd2254c62a7c16be85cd850bc4eeb1c87
Wei Mo <miloweimo@outlook.com>
2023-08-22 15:29:04 +0800

    ref:lint
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/learn-vue2/src/store/index.js
+++ packages/learn-vue2/src/store/index.js
@@ -5,35 +5,9 @@ import jsonServerService from '../services/jsonServer'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    servertime: '',
-    auth: {},
-    notes: [],
-  },
-  mutations: {
-    SET_AUTH(state, code) {
-      state.auth = code
-    },
-    setServertime(state, ts) {
-      state.servertime = ts;
-    },
-    addServertime(state, ms) {
-      state.servertime = state.servertime + ms;
-    },
-    SET_NOTES(state, notes) {
-      state.notes = notes
-    },
-  },
-  actions: {
-    async updateAllNotes() {
-      console.log('actions:updateAllNotes');
-      const notes = await jsonServerService.getAllNotes()
-      console.log('notes:',notes);
-      
-      this.commit('SET_NOTES', notes)
-    }
-
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   getters: {},
   modules: {},
 });
