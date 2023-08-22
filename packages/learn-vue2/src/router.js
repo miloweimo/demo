7e608e88217cef3b01f9a26764e3815a89fc0a6a
Wei Mo <miloweimo@outlook.com>
2023-08-22 15:25:26 +0800

    style:lint
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/learn-vue2/src/router.js
+++ packages/learn-vue2/src/router.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './modules/home'
-// import Dashboard from './components/Dashboard.vue'
-import Hello from './components/Hello.vue'
-import Font from './components/Font.vue'
-import About from './components/About.vue'
-import Grids from './components/Grids.vue'
-import Flex from './components/Flex.vue'
-import Layout from './components/Layout.vue'
-import Highchart from './components/Highchart.vue'
-import Getdata from './components/Getdata.vue'
 
 Vue.use(Router)
 
@@ -21,49 +12,8 @@ export default new Router({
       component: Home
     },
     {
-      path: '/hello',
-      name: 'hello',
-      component: Hello
-    },
-    {
-      path: '/font',
-      name: 'font',
-      component: Font
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '/grids',
-      name: 'grids',
-      component: Grids
-    },
-    {
-      path: '/flex',
-      name: 'flex',
-      component: Flex
-    },
-    {
-      path: '/layout',
-      name: 'layout',
-      component: Layout
-    },
-    {
-      path: '/highchart',
-      name: 'highchart',
-      component: Highchart
-    },
-    {
-      path: '/getdata',
-      name: 'getdata',
-      component: Getdata
-    },
-    {
       path: '/testpage',
       name: 'testpage',
-      // component: () => import('./modules/testpage/IndexPage.vue')
       component: resolve => require(['@/modules/testpage'], resolve)
     },
   ]
