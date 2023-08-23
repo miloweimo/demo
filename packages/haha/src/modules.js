9f80db30a413f3baab4ec0c1ebd97abe449c43fb
Wei Mo <miloweimo@outlook.com>
2023-08-23 16:18:27 +0800

    ref:
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/haha/src/modules.js
+++ packages/haha/src/modules.js
@@ -8,6 +8,14 @@ export const routerModules = {
   comploop: require.context('./modules/comploop', true, /\broutes\b\.js$/),
 };
 
+export const navModules = [
+  { title: "Home", icon: "mdi-home-variant-outline", linkto: "/" },
+  // { title: "Demo", icon: "mdi-teddy-bear", linkto: "/demo" },
+  // { title: "Test", icon: "mdi-test-tube", linkto: "/test" },
+  { title: "Modal 模态弹窗", icon: "mdi-message-badge-outline", linkto: "/modal" },
+  { title: "组件循环依赖", icon: "mdi-sync", linkto: "/comploop" },
+];
+
 export const storeModules = {
   store: require.context('./store/modules', true, /[A-Za-z0-9-_,\s]+\.js$/i),
   demo: require.context('./modules/demo/store', true, /[A-Za-z0-9-_,\s]+\.js$/i),
@@ -17,11 +25,3 @@ export const localeModules = {
   locales: require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i),
   demo: require.context('./modules/demo/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i),
 };
-
-export const navModules = [
-  { title: "Home", icon: "mdi-home-variant-outline", linkto: "/" },
-  // { title: "Demo", icon: "mdi-teddy-bear", linkto: "/demo" },
-  // { title: "Test", icon: "mdi-test-tube", linkto: "/test" },
-  { title: "Modal 模态弹窗", icon: "mdi-message-badge-outline", linkto: "/modal" },
-  { title: "组件循环依赖", icon: "mdi-sync", linkto: "/comploop" },
-];
