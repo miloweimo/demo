adb1d07c72c04accb01ea461a62dff50e4088589
Wei Mo <miloweimo@outlook.com>
2023-08-23 15:38:01 +0800

    ref:
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/haha/src/modules.js
+++ packages/haha/src/modules.js
@@ -3,22 +3,18 @@
 export const routerModules = {
   default: require.context('./modules/default', true, /\broutes\b\.js$/),
   demo: require.context('./modules/demo', true, /\broutes\b\.js$/),
-  test: require.context('./modules/test', true, /\broutes\b\.js$/),
+  // test: require.context('./modules/test', true, /\broutes\b\.js$/),
   modal: require.context('./modules/modal', true, /\broutes\b\.js$/),
 };
 
 export const storeModules = {
   store: require.context('./store/modules', true, /[A-Za-z0-9-_,\s]+\.js$/i),
   demo: require.context('./modules/demo/store', true, /[A-Za-z0-9-_,\s]+\.js$/i),
-  // test: require.context('./modules/test/store', true, /[A-Za-z0-9-_,\s]+\.js$/i),
-  // modal: require.context('./modules/modal/store', true, /[A-Za-z0-9-_,\s]+\.js$/i),
 };
 
 export const localeModules = {
   locales: require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i),
   demo: require.context('./modules/demo/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i),
-  // test: require.context('./modules/test/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i),
-  // modal: require.context('./modules/modal/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i),
 };
 
 export const navModules = [
