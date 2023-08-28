2ebce17f03131edb04048b9a6820d90fbe5f28cb
Wei Mo <miloweimo@outlook.com>
2023-08-28 17:38:39 +0800

    主页链接
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/haha/src/modules/default/index.vue
+++ packages/haha/src/modules/default/index.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <h1>Default</h1>
+    <h1>Demo</h1>
+    <a 
+      v-for="item,key in navModules"
+      :key="key"
+      class="mr-2"
+      :href="`#${item.linkto}`"
+    >
+      {{ item.title }}
+    </a>
   </div>
 </template>
 
 <script>
-export default {
+import {navModules} from "@/modules";
 
+export default {
+  data() {
+    return {
+      navModules
+    }
+  }
 }
 </script>
 
