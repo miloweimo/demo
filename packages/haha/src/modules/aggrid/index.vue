806ea21735f455e5ebe16a4bfcc222af351ebe97
Wei Mo <miloweimo@outlook.com>
2023-09-06 15:21:19 +0800

    添加aggrid
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/haha/src/modules/aggrid/index.vue
+++ packages/haha/src/modules/aggrid/index.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
     <h1>aggrid</h1>
+    <!-- npm install --save ag-grid-community ag-grid-vue vue-property-decorator@^8.0.0 -w @demo/haha -->
+    <hr class="my-2">
+    <ag-grid-vue style="height: 500px;"
+        class="ag-theme-alpine"
+        :columnDefs="columnDefs"
+        :rowData="rowData">
+    </ag-grid-vue>
+    <hr class="my-2">
   </div>
 </template>
-
 <script>
+import { AgGridVue } from "ag-grid-vue";
 
 export default {
-}
+  name: "Aggrid",
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null,
+    };
+  },
+  components: {
+    AgGridVue,
+  },
+  beforeMount() {
+    this.columnDefs = [
+      { field: "make" },
+      { field: "model" },
+      { field: "price" },
+    ];
+
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxster", price: 72000 },
+    ];
+  },
+};
 </script>
-
-<style>
-
+<style lang="scss">
+  @import "~ag-grid-community/styles/ag-grid.css";
+  @import "~ag-grid-community/styles/ag-theme-alpine.css";
 </style>
