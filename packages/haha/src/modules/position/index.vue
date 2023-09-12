e6b38f4a215a89c479c53c5f2655ece48abc0717
Wei Mo <miloweimo@outlook.com>
2023-09-12 16:31:29 +0800

    feat(position): 拖拽盒子
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- packages/haha/src/modules/position/index.vue
+++ packages/haha/src/modules/position/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div style="position: relative;border: 1px solid;" :style="`height: ${height - 80}px;`" @mousemove="handleDragBox" @mouseup="isSelectBox = false">
     <h1>元素位置</h1>
-    <div>
-      <p>{{ window?.innerWidth || document?.documentElement?.clientWidth || document?.body?.clientWidth }}</p>
-      <p>{{ window?.innerHeight || document?.documentElement?.clientHeight || document?.body?.clientHeight }}</p>
-      <p>视口大小: {{ viewport }} 实时: {{ viewportRealtime }}</p>
+    <p>{{ window?.innerWidth || document?.documentElement?.clientWidth || document?.body?.clientWidth }}
+    {{ window?.innerHeight || document?.documentElement?.clientHeight || document?.body?.clientHeight }}
+    视口大小: {{ viewport }} 实时: {{ viewportRealtime }}</p>
+    <div ref="box" class="box" @mousedown="isSelectBox = true">
+      {{ boxx }} {{ boxy }}
     </div>
   </div>
 </template>
@@ -18,7 +19,12 @@ export default {
     return {
       window,
       document,
+      height: 0,
+      width: 0,
       viewportRealtime: '',
+      isSelectBox: false,
+      boxx: 0,
+      boxy: 0,
     };
   },
   created() {
@@ -26,6 +32,7 @@ export default {
   },
   mounted() {
     this.viewportRealtime = this.getViewport();
+    this.getBoxPosition();
   },
   destroyed() {
     window.removeEventListener("resize", this.resizeHandler);
@@ -40,9 +47,9 @@ export default {
   },
   methods: {
     getViewport() {
-      const width = document.body.clientWidth;
-      const height = document.body.clientHeight;
-      const viewport = `${width} x ${height}`;
+      this.width = document.body.clientWidth;
+      this.height = document.body.clientHeight;
+      const viewport = `${this.width} x ${this.height}`;
       return viewport;
     },
     // 防抖
@@ -63,11 +70,37 @@ export default {
     resizeHandler() {
       this.myDebounce(() => {this.viewportRealtime = this.getViewport()}, 300)();
     },
+    // 获取box 元素位置 boxx boxy offsetLeft offsetTop
+    getBoxPosition() {
+      this.boxx = this.$refs.box.offsetLeft;
+      this.boxy = this.$refs.box.offsetTop;
+    },
+    handleDragBox(e) {
+      if(this.isSelectBox) {
+        console.log(e.movementX, e.movementY);
+        const boxElement = this.$refs.box;
+        this.boxx = this.$refs.box.offsetLeft;
+        this.boxy = this.$refs.box.offsetTop;
+        boxElement.style.left = `${this.boxx + e.movementX}px`;
+        boxElement.style.top = `${this.boxy + e.movementY}px`;
+      }
+    }
   },
 
 }
 </script>
 
 <style>
-
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: rgba(239, 178, 64, 0.869);
+  /* 抓手 */
+  cursor: grab;
+  position: absolute;
+}
+/* 按下抓住 */
+.box:active {
+  cursor: grabbing;
+}
 </style>
